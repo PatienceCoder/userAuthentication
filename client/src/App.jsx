33248bdfb96e6d49bf4aded8227d91b01a4a7d13
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-06-04 11:13:41 +0530

    added an endpoint for sending email messages and added a feature to check user has internet access or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,29 @@ import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Forgotpassword from './components/Forgotpassword';
 import { useAuthContext } from '../authContext/authContext';
-
+import { useEffect, useState } from 'react';
 function App() {
-  const {authenticatedUser} = useAuthContext()
+  const {authenticatedUser} = useAuthContext();
+  const [onlineStatus,setOnlineStatus] = useState(navigator.onLine);
+  useEffect(() => {
+    function setOnline(){
+      setOnlineStatus(true)
+    }
+    function setOffline(){
+      setOnlineStatus(false)
+    }
+    window.addEventListener('online',setOnline)
+    window.addEventListener('offline',setOffline)
+  })
   return (
     <>
+    {
+      !onlineStatus && (
+        <div id='no-internet-connection'>
+          No Internet Connection
+        </div>
+      )
+    }
     <Routes>
         <Route path='/' element={<Register/>} />
         <Route path='/verification' element={<Verification/>} />
